f63cf682dae35de772d6bb76b6030bd8f51a3cc3
Freya-Mar <d1041231078@student.untan.ac.id>
2026-02-12 22:54:19 +0700

    Add files via upload

---
+++ Pertemuan 1/CodePertama.java
@@ -0,0 +1,9 @@
+public class CodePertama {
+    public static void main(String[] args){
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalah program Java pertama saya.");
+    }
+}
+/* Nama : Freya Martines */
+/* Nim : D1041231078 */
+/* Tanggal : 5 Februari 2025 */
