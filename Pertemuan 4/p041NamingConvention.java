33d9deb3bca6d8b336e9ad361e9ff856a71430fd
Freya-Mar <d1041231078@student.untan.ac.id>
2026-02-13 08:48:46 +0700

    Add files via upload

---
+++ Pertemuan 4/p041NamingConvention.java
@@ -0,0 +1,19 @@
+public class p041NamingConvention {
+
+    public static final double PI = 3.14;
+
+    public static void main(String[] args) {
+
+        //Variable dengan camelcase
+        int studentAge = 20;
+        String studentName = "Budi";
+        boolean isActive = true;
+
+        System.out.println("Nama: " + studentName);
+        System.out.println("Umur: " + studentAge);
+        System.out.println("Status: " + isActive);
+        System.out.println("Konstanta PI: " + PI);
+
+
+    }
+}
