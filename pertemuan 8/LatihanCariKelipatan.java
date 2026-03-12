b8c6534ef44f2a1ddb776b1334b6693644ff723a
Freya-Mar <d1041231078@student.untan.ac.id>
2026-03-12 11:12:01 +0700

    first commit

---
+++ pertemuan 8/LatihanCariKelipatan.java
@@ -0,0 +1,18 @@
+public class LatihanCariKelipatan {
+
+    public static void main(String[] args) {
+        
+        System.out.println("=== CARI KELIPATAN 7 ===");
+
+        for(int i=1; i<= 50; i++) {
+            System.out.println("Cek : " + i);
+
+            if(i % 7 == 0) {
+                System.out.println("Kelipatan 7 Pertama: " + i);
+                break;
+            }
+        }
+
+        System.out.println("Pencarian selesai. ");
+    }
+}
